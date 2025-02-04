7fcdb152f2842cf0e874b78cb23dd1af6c7f78a5
Julian <jacaminaur@gmail.com>
2025-02-04 08:48:12 +0100

    Custom Slider

--- script.js
+++ script.js
@@ -1,71 +1,28 @@
+const toggle = document.getElementById("payment");
+const body = document.querySelector("body");
+const slider = document.querySelector(".slider__");
+const switchBTN = document.querySelector(".switch .slider");
 
-  const toggle = document.getElementById('payment');
-  const body = document.querySelector('body');
-  const slider = document.querySelector('.slider__');
-  const sliderBtn = document.querySelector('.img-slider-div');
-  const emptySlider = document.querySelector('.empty-slider');
-  leftClick = false;
+const inputRange = document.querySelector(".input-range");
+const sliderBtn = document.querySelector(".img-slider-div");
 
-  toggle.addEventListener('click',()=>{
-    body.dataset.active = (toggle.checked) ? 'true' : 'false';
-  });
+toggle.addEventListener("click", () => {
+  body.dataset.active = toggle.checked ? "true" : "false";
+});
 
-  // slider.onmousedown = e => handleOnDown(e);
+switchBTN.addEventListener("mousedown", () => {
+  switchBTN.style.cursor = "grabbing";
+});
+switchBTN.addEventListener("mouseup", () => {
+  switchBTN.style.cursor = "grab";
+});
 
-  // slider.ontouchstart = e => handleOnDown(e.touches[0]);
+function updateThumb() {
+  const percent =
+    (inputRange.value - inputRange.min) / (inputRange.max - inputRange.min);
+  const thumbPosition = percent * inputRange.offsetWidth;
+  sliderBtn.style.left = `${thumbPosition}px`;
+}
 
-  // slider.onmouseup = e => handleOnUp(e);
-
-  // slider.ontouchend = e => handleOnUp(e.touches[0]);
-
-  // slider.onmousemove = e => handleOnMove(e);
-
-  // slider.ontouchmove = e => handleOnMove(e.touches[0]);
-
-  // emptySlider.onmousedown = e => {
-  //   const mouseDelta = parseFloat(emptySlider.dataset.mouseDownAt) - e.clientX; // posicion del mouse 
-  //   const maxDelta = parseInt(emptySlider.clientWidth);
-  //   const percentage = (mouseDelta / maxDelta) * -100;
-  //   const nextPercentageUnconstrained = parseFloat(emptySlider.dataset.prevPercentage) + percentage;
-  //   const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  //   console.log('mouseDelta: ' + mouseDelta);
-  //   console.log('e.clientX: '+ e.clientX);
-  //   console.log('maxDelta: ' + maxDelta);
-  //   console.log('percentage: '+ nextPercentage);
-  //   console.log('\n');
-  // };
-
-
-  // const handleOnDown = e => {
-  //   leftClick = true;
-    
-  // }
-  // const handleOnUp = () => {
-  //   slider.dataset.mouseDownAt = "0";  
-  //   slider.dataset.prevPercentage = slider.dataset.percentage; 
-  //   leftClick = false;
-  //   // guardamos el porcentaje del ultimo scroll
-  // }
-
-  // const handleOnMove = e => {
-  //   if (!leftClick) return;
-  //   if(slider.dataset.mouseDownAt === "0") return; // Si no se ha cliqueado que retorne
-  
-  //   const mouseDelta = parseFloat(slider.dataset.mouseDownAt) - e.clientX; // posicion del mouse 
-  //   const maxDelta = slider.innerWidth / 1.5;
-
-    
-  //   const percentage = (mouseDelta / maxDelta) * -100;
-  //   const nextPercentageUnconstrained = parseFloat(slider.dataset.prevPercentage) + percentage;
-  //   const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-    
-  //   slider.dataset.percentage = nextPercentage;
-    
-  //   slider.animate({
-  //     transform:`translate(-${55}%, 0%)`
-  //   },{
-  //     duration: 500,
-  //     fill:"forwards"
-  //   });
-    
-  // }
+inputRange.addEventListener("input", updateThumb);
+window.addEventListener("load", updateThumb);
